aee505af06797251f581c859d20cbb408835816f
sourab <Sourabkapoor@yahoo,com>
2021-11-06 00:06:46 +0530

    Added from lenovo laptop

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NavBar from './Components/NavBar/NavBar';
 import Music from './Components/Music/Music';
 import Contact from './Components/Contact/Contact';
 //import Test from './test.js';
+import ReactPlayer from 'react-player/soundcloud'
 
 class App extends React.Component {
   constructor(props) {
@@ -19,6 +20,10 @@ class App extends React.Component {
       <NavBar/>
       <br/>
       <Music/> 
+      <ReactPlayer url='https://soundcloud.com/miami-nights-1984/accelerated' show_playcoun="false"
+        className='w-75 center ma4'
+      />
+      <audio src='https://soundcloud.com/miami-nights-1984/accelerated' autoPlay/>
       {
         //<Test/>
       }
